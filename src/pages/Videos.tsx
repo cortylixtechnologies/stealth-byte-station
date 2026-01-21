8d891c18b4b29a7d3e24102108387155c1ac4e3d
cortylixtechnologies <cortylixtechnologies@gmail.com>
2026-01-21 15:14:17 +0300

    Update Videos.tsx

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -66,7 +66,7 @@ const Videos = () => {
   return (
     <div className="min-h-screen bg-background matrix-bg">
       <Navbar />
-      <WhatsAppButton phoneNumber="1234567890" />
+      <WhatsAppButton phoneNumber="+25576222330" />
 
       <main className="pt-24 pb-20 px-4">
         <div className="container mx-auto">
